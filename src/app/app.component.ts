2d8eba1614f2b87eed4ae79709e653df3d0ef8d1
Liliana <liliana.igm@gmail.com>
2025-04-08 20:28:14 -0600

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+[x: string]: any;
   title = 'angular-testing-project';
 
-  addTask(task: string) {
+  elemento:string[]=[]
+  nuevo:string='';
+  eliminar:string='';
 
+  addTask() {
+    this.elemento.push(this.nuevo)
+    this.nuevo='';
   }
 
-  deleteTask(task: string) {
+  deleteTask() {
+    this.elemento.pop()
+    this.eliminar='';
   }
 
   getTasks(): string[] {
